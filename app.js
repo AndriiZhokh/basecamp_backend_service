7654c8c095c1ce9f799844e4a8877f4130a923c8
Andriy7172 <andriyko.zoh5@gmail.com>
2019-02-14 22:22:32 +0200

    Adding new method which create new show in database

--- app.js
+++ app.js
@@ -1,41 +1,61 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import fileUpload from 'express-fileupload';
+import multer from 'multer';
+import path from 'path';
+import { serverPort } from './config/config.json';
 
-import { serverPort } from './config/config.json'
-import * as db from './models/db';
+import Show from './models/Show';
+import Season from './models/Season';
+import Episode from './models/Episode';
+import DB from './models/db';
 
-db.DBconection();
+//Set Storage Engine
+const storage = multer.diskStorage({
+  destination: 'public/images/',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+});
+
+//Init upload
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb)
+  }
+}).single('image');
+
+//Check File Type
+const checkFileType = (file, cb) => {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true)
+  } else {
+    cb('Error: Images only')
+  }
+}
+
+//Init app
 const app = express();
-app.use(fileUpload());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-app.get('/users', (req, res) => {
+app.get('/get_show', (req, res) => {
   db.showAll().then(data => res.json(data))
               .catch(() => console.log('error'));
 });
 
-app.post('/show', (req, res) => {  
-  console.log(`data from client - ${JSON.stringify(req.body)}`);
-});
-
-app.post('/upload', (req, res) => {
-  console.log(req.files.file.name);
-  /* let uploadFile = req.files.file;
-  const fileName = req.files.file.name;
-  uploadFile.mv(`${__dirname}/public/images/${fileName}`, 
-  function(err) {
-    if(err) {
-      return res.status(500).send(err);
+app.post('/add_show', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.json({msg: err})
+    } else {
+      console.log(typeof req.file.path);
+      const show = new Show(req);
+      show.addShow();
+      res.json({test: 'test'});
     }
-    res.json({
-      file: `public/${req.files.file.name}`
-    });
-  }) */
+  });
 });
 
 const server = app.listen(serverPort, () => {
